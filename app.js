7e7f4811f3d8c8976a49be2d0b1ea05ae31fb03b
石川 雄基 / ほと <yuuki.ishikawa@gaiax.com>
2017-02-03 19:19:53 +0900

    redis session

--- app.js
+++ app.js
@@ -7,4 +7,17 @@ app.set('io', io);
 const db = require('mysql').createPool( process.env.MYSQL_URL || 'mysql://root@localhost/flourish' );
 app.set('db', db);
 
+const redis = require('redis').createClient( process.env.REDIS_URL );
+app.set('redis', redis);
+
+const session = require('express-session');
+const RedisStore = require('connect-redis')(session);
+app.use(session({
+  store: new RedisStore({ client: redis }),
+  secret: process.env.SESSION_SECRET || 'flourish',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: true },
+}));
+
 module.exports = http;
