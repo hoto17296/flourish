67de5a52130e4d663e1d94f443ee86d5834ce139
石川 雄基 / ほと <yuuki.ishikawa@gaiax.com>
2017-02-03 18:36:05 +0900

    mysql

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require('express');
+const mysql = require('mysql');
 
 const app = express();
+
+const db = mysql.createPool( process.env.MYSQL_URL || 'mysql://root@localhost/flourish' );
+app.set('db', db);
 
 module.exports = app;
