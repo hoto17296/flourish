90d474fb9cacba459b0be7fdb90edf0114290a9f
石川 雄基 / ほと <yuuki.ishikawa@gaiax.com>
2017-02-05 20:02:24 +0900

    move create method to orm

--- models/user.js
+++ models/user.js
@@ -2,36 +2,12 @@ const ORM = require('../lib/orm');
 const mysql = require('../stores/mysql');
 
 class User extends ORM {
-  static create(params) {
-    return new Promise((resolve, reject) => {
-      mysql.getConnection((err, conn) => {
-        if (err) return reject(err);
-        try {
-          const user = new this(params);
-          const sql = "insert into users (name,email,uid) values (?,?,?);";
-          conn.query(sql, [ user.name, user.email, user.uid ], (err) => {
-            if (err) return reject(err);
-            const sql = "SELECT LAST_INSERT_ID() AS id;";
-            conn.query(sql, [], (err, result) => {
-              if (err) return reject(err);
-              user.id = result[0].id;
-              resolve(user);
-              conn.release();
-            });
-          });
-        }
-        catch(err) {
-          conn.release();
-          reject(err);
-        }
-      });
-    });
-  }
 }
 
 User._table = {
   name: 'users',
   pk: 'id',
+  schema: ['id', 'name', 'email', 'organization', 'uid', 'is_admin', 'notify'],
 }
 
 module.exports = User;
